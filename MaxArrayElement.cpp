1aad11dc00dc4ae50d61386757a0211609992440
Shashank Tiwari <67553585+sanktiwarirobotics@users.noreply.github.com>
2022-07-16 15:34:41 +0530

    Create MaxArrayElement.cpp

---
+++ MaxArrayElement.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int arr[]={-4,-6,-9,5};
+int i;
+int max=arr[1];
+// cout<<arr[0];
+for(i=0;i<3;i++){
+    
+    
+    if(arr[i]>max)
+    
+    max=arr[i];
+
+}
+    
+
+    cout<<max;
+
+
+  
+}
